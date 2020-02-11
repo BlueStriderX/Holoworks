a43b94e82614ab014f58d51e6e53084c3d26a411
TheDerpGamer <garret2kool@gmail.com>
2020-02-11 09:54:46 -0700

    Update DataStreamer.java

--- src/main/java/net/dovtech/holoworks/blocks/DataStreamer.java
+++ src/main/java/net/dovtech/holoworks/blocks/DataStreamer.java
@@ -1,17 +1,38 @@
 package net.dovtech.holoworks.blocks;
 
-import api.element.Recipe;
 import api.element.block.Block;
+import api.listener.type.ServerListener;
+import org.schema.game.client.controller.manager.ingame.PlayerGameControlManager;
+import org.schema.game.client.controller.manager.ingame.structurecontrol.StructureControllerManager;
+import org.schema.game.client.data.PlayerControllable;
+import org.schema.game.common.controller.PlayerUsableInterface;
+import org.schema.game.common.data.player.ControllerStateUnit;
+import org.schema.game.common.data.player.PlayerCharacter;
+import org.schema.game.common.data.player.PlayerState;
+import org.schema.game.common.data.world.SimpleTransformableSendableObject;
+import org.schema.game.server.data.element.ServerWorld;
+import org.schema.schine.ai.AiEntityStateInterface;
+import org.schema.schine.network.objects.NetworkEntity;
+import org.schema.schine.network.server.ServerController;
 
 public class DataStreamer extends Block {
+    private enum systemEnum {
+        REACTOR,
+        REACTOR_CHAMBER,
+        SHIELD_RECHARGE,
+        SHIELD_CAPACITY,
+        WEAPON_COMPUTER,
+    }
+    private systemEnum systemType;
+
     public DataStreamer() {
         super(3301, "DATA_STREAMER");
-
-        //Recipe Stuff
-        Recipe recipe = new Recipe();
-        recipe.setIngredients();
-        this.setRecipe();
     }
 
+    @ServerListener
+    public void onPlaceBlock(PlayerState player, Block block) {
+        if(block.getID() == this.getID()) {
 
+        }
+    }
 }
